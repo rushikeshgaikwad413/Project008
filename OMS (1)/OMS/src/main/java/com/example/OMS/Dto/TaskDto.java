82a4cc3f6a47fb3311586bfd2a2953ec0574f1be
veenasarode <vu2vmn@gmail.com>
2023-12-28 17:02:20 +0530

    dto add

--- OMS (1)/OMS/src/main/java/com/example/OMS/Dto/TaskDto.java
+++ OMS (1)/OMS/src/main/java/com/example/OMS/Dto/TaskDto.java
@@ -1,2 +1,19 @@
-package com.example.OMS.Dto;public class TaskDto {
+package com.example.OMS.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TaskDto {
+
+    private Long taskId;
+    private String taskName;
+    private String description;
+    private Long assignedTo;
+    private String status;
+    private String duration;
+
 }
--- OMS (1)/OMS/src/main/java/com/example/OMS/Dto/TokenDto.java
+++ OMS (1)/OMS/src/main/java/com/example/OMS/Dto/TokenDto.java
@@ -1,2 +1,25 @@
-package com.example.OMS.Dto;public class TokenDto {
+package com.example.OMS.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private Long tokenId;
+    private String taskName;
+    private String description;
+    private String status;
+    private String duration;
+    private Long assignedToUserId;
+    private LocalDate createDate;
+    private LocalDate endDate;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
